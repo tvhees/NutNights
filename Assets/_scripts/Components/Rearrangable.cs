c22c249487f6fd1dcaedc6b1f4cf7a8503f1d4fb
Torbjorn van Heeswijck <t.vanheeswijck@gmail.com>
2017-02-13 21:12:54 +1030

    fixed #1 drag and drop working

--- Assets/_scripts/Components/Rearrangable.cs
+++ Assets/_scripts/Components/Rearrangable.cs
@@ -1,17 +1,21 @@
 ﻿using Collections;
+using GameStates;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Components
 {
-    public class Rearrangable : MonoBehaviour, IDropHandler, IBeginDragHandler, IDragHandler
+    public class Rearrangable : MonoBehaviour, IBeginDragHandler, IDropHandler, IDragHandler, IEndDragHandler
     {
-        private int startIndex;
-        private int finishIndex;
+        private Button button;
+        private CanvasGroup canvasGroup;
+        private Vector2 startPosition;
 
-        public void OnBeginDrag(PointerEventData eventData)
+        public void OnEnable()
         {
-            startIndex = transform.GetSiblingIndex();
+            button = GetComponent<Button>();
+            canvasGroup = GetComponent<CanvasGroup>();
         }
 
         public void OnDrop(PointerEventData eventData)
@@ -22,9 +26,23 @@ namespace Components
             collection.SwapCards(i, j);
         }
 
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            button.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+            startPosition = transform.position;
+        }
+
         public void OnDrag(PointerEventData eventData)
         {
             transform.position = eventData.position;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            button.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+            transform.position = startPosition;
         }
     }
 }
