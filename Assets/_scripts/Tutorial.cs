8b4499ef6f6ec1160b7c6ec498fe6d4eb9161051
Torbjorn van Heeswijck <t.vanheeswijck@gmail.com>
2017-02-18 22:58:58 +1030

    added menu and tutorial scenes with buttons to load them

---
+++ Assets/_scripts/Tutorial.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    public int Seed;
+
+    private void Awake()
+    {
+        Random.InitState(Seed);
+        SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/_scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour {
+
+        public void StartNewGame()
+        {
+            SceneManager.LoadSceneAsync("Game");
+        }
+
+        public void StartTutorialGame()
+        {
+            SceneManager.LoadSceneAsync("Tutorial");
+        }
+    }
+}
